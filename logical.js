6bc01010025ab485549070db7b3ca32b3bb6999f
Tracy-miranja <barazatracy44@gmail.com>
2023-09-21 15:24:19 +0300

    Logical operator

---
+++ logical.js
@@ -0,0 +1,3 @@
+if(hour<4 || hour> 10){
+    alert ('we are on track')
+}
--- type.js
+++ type.js
@@ -17,9 +17,22 @@ const another= Object.assign({}, circle)
 
  //memory alocation and de-alocation done by Garbage collector
  //built in object.
- Math.abs(return absolute number)
- Math.random
- Math.max(largest number in the list)
- MessageChannel.includes('mine')
- MessageChannel.replace('fist', 'sec')
+//  Math.abs(return absolute number)
+//  Math.random
+//  Math.max(largest number in the list)
+//  string object
+//  MessageChannel.includes('mine')
+//  MessageChannel.replace('fist', 'sec')
+//  MessageChannel.trim()
+//  MessageChannel.split()
+
+ //template literals
+ const name="John";
+ const anothe= ` Hi ${name} 
+Thanks for reaching out
+regards
+Tracy miranja
+
+ `
+ console.log(anothe);
 
